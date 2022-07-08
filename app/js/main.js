d3d466459d4f326d3a2cd39398a69325c329241d
Zerdestr <ajiekc1004@gmail.com>
2022-07-08 20:45:20 +0300

    almost finished styling the product page

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,19 @@ $(function () {
       }
     })
 
+    .on('click', '.info-tabs__btn', function (event) {
+      event.preventDefault();
+      if (!$(this).hasClass('info-tabs__btn--active')) {
+        let tabId = $(this).attr("rel");
+
+        $(".info-tabs__content").removeClass("info-tabs__content--active");
+        $('.info-tabs__btn').removeClass("info-tabs__btn--active");
+
+        $(this).addClass("info-tabs__btn--active");
+        $("#" + tabId).addClass("info-tabs__content--active");
+      }
+    })
+
     .on('click', '.menu__burger', function () {
       $('body').addClass('no-scroll');
       $('.mobile-header').addClass('mobile-menu--active');
@@ -32,7 +45,7 @@ $(function () {
     })
 
 
-  $(".product__raiting").rateYo({
+  $(".raiting").rateYo({
     starSvg: '<svg width = "16" height = "16" viewBox = "0 0 16 16" fill = "none" xmlns = "http://www.w3.org/2000/svg" >' +
       '<path d="M0.022973 6.16432C0.0780978 5.9946 0.224753 5.87091 0.401315 5.84529L5.36139 5.12451L7.57966 0.629933C7.6586 0.469933 7.82157 0.368652 7.99997 0.368652C8.17841 0.368652 8.34135 0.469933 8.42032 0.629933L10.6387 5.12451L15.5987 5.84529C15.7752 5.87091 15.9219 5.9946 15.977 6.16429C16.0322 6.334 15.9862 6.52028 15.8584 6.64482L12.2694 10.1434L13.1165 15.0834C13.1467 15.2593 13.0744 15.437 12.9301 15.5419C12.8484 15.6012 12.7517 15.6314 12.6545 15.6314C12.5799 15.6314 12.505 15.6136 12.4365 15.5776L8 13.2451L3.56374 15.5775C3.40577 15.6606 3.21443 15.6467 3.07009 15.5419C2.92574 15.437 2.8534 15.2593 2.88356 15.0834L3.73096 10.1434L0.141567 6.64478C0.0138172 6.52028 -0.0322143 6.334 0.022973 6.16432Z"</path>' +
       '</svg >',
@@ -105,29 +118,60 @@ $(function () {
     },
 
     navigation: {
-      nextEl: '.reviews-swiper__button--next',
-      prevEl: '.reviews-swiper__button--prev',
+      nextEl: '.swipers__buttons--next',
+      prevEl: '.swipers__buttons--prev',
     }
   });
 
-  // let productSwiper = new Swiper('.product-swiper', {
+  let interestedSwiper = new Swiper('.interested-swiper', {
 
-  //   // autoplay: {
-  //   //   delay: 5000,
-  //   // },
+    autoplay: {
+      delay: 5000,
+    },
 
-  //   // wrapperClass: 'product-swiper__wrapper',
+    spaceBetween: 30,
 
-  //   // slideClass: 'product-swiper__slide',
+    slidesPerView: 5,
 
-  //   direction: 'horizontal',
-  //   loop: true,
+    wrapperClass: 'interested-swiper__wrapper',
 
-  //   // navigation: {
-  //   //   nextEl: '.reviews-swiper__button--next',
-  //   //   prevEl: '.reviews-swiper__button--prev',
-  //   // }
-  // });
+    slideClass: 'interested-swiper__slide',
+
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+      el: '.swipers__pagination',
+      type: 'bullets',
+      bulletClass: 'swipers__pagination-bullet',
+      bulletActiveClass: 'swipers__pagination-bullet--active',
+      clickable: true
+    },
+
+    navigation: {
+      nextEl: '.swipers__buttons--next',
+      prevEl: '.swipers__buttons--prev',
+    }
+  });
+
+  let productSwiper = new Swiper('.product-swiper', {
+
+    autoplay: {
+      delay: 5000,
+    },
+
+    wrapperClass: 'product-swiper__wrapper',
+
+    slideClass: 'product-swiper__slide',
+
+    direction: 'horizontal',
+    loop: true,
+
+    navigation: {
+      nextEl: '.swipers__buttons--next',
+      prevEl: '.swipers__buttons--prev',
+    }
+  });
 
   let restaurantsSlider = null;
   let mediaQuerySize = 992;
